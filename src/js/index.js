a06eba2246b4b8bfae1f5094647248cf117a265e
diogo-kappaun <115656394+diogo-kappaun@users.noreply.github.com>
2023-03-21 21:38:32 -0300

    creating-script

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+const botaoGerador = document.querySelector('#dice')
+const idDoConselho = document.querySelector('#advice-id')
+const descricaoDoConselho = document.querySelector
+('#advice')
+
+botaoGerador.addEventListener('click', () => {
+  alterarElementos();
+})
+
+async function gerarUmConselhoAleatorio() {
+  const resposta = await fetch("https://api.adviceslip.com/advice");
+  return await resposta.json();
+}
+
+async function alterarElementos() {
+  const conselho = await gerarUmConselhoAleatorio();
+  idDoConselho.innerHTML = `${conselho.slip.id}`;
+  descricaoDoConselho.innerHTML = `${conselho.slip.advice}`;
+}
